6b4f68924fe018125a953636d4719e7f4e6d2ae1
Burak Çetin <burakcetin9724@gmail.com>
2025-06-29 14:14:11 +0300

    Add files via upload

---
+++ _document.tsx
@@ -0,0 +1,35 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="tr">
+      <Head>
+        {/* DNS prefetch for performance */}
+        <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+        <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+        
+        {/* Security headers */}
+        <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
+        <meta httpEquiv="X-Frame-Options" content="DENY" />
+        <meta httpEquiv="X-XSS-Protection" content="1; mode=block" />
+        <meta httpEquiv="Referrer-Policy" content="strict-origin-when-cross-origin" />
+        
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W3X2MLGDX3"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W3X2MLGDX3');
+          `
+        }} />
+      </Head>
+      <body className="font-inter">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
